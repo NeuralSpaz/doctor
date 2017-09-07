a60f478d5233b3031fbe559dd88db512899b7812
josh <josh@zool.com>
2017-09-08 00:34:06 +1000

    minimum

--- doctor.go
+++ doctor.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func init() {
+	flag.StringVar(&token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+var token string
 
 func main() {
 	fmt.Println("Discord Bot")
+
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("Error creating Discord session: ", err)
+		return
+	}
+
+	discord.AddHandler(ready)
+
+	// discord.AddHandler(messageCreate)
+
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("Error opening Discord session: ", err)
+	}
+
+	// discord.
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Airhorn is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	discord.Close()
+
+}
+
+func ready(s *discordgo.Session, event *discordgo.Ready) {
+
+	// Set the playing status.
+	s.UpdateStatus(0, "!doctor")
 }
